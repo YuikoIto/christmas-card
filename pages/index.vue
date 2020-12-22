64876ea729b58139cb673c9af115c4b24f6b143e
YuikoIto <toffywing@gmail.com>
2020-12-23 03:28:02 +0900

    svgのレイアウト調整

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,31 @@
   <section class="container">
     <div>
       <div>
-        <svg ref="svgArea" viewBox="0 0 200 100">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          version="1.1"
+          ref="svgArea"
+          viewBox="0 0 200 100"
+        >
           <rect
+            rx="20"
             x="0"
             y="0"
             width="200"
             height="100"
             fill="#fff"
-            stroke="#12b886"
+            stroke="#008037"
             stroke-width="15"
           ></rect>
+          <circle cx="10" cy="10" r="10" fill="#38b6ff" />
+          <circle cx="200" cy="20" r="10" fill="#F44D61" />
+          <circle cx="150" cy="10" r="10" fill="#F44D61" />
+          <circle cx="100" cy="100" r="10" fill="#5ce1e6" />
+          <circle cx="0" cy="80" r="10" fill="#ff5757" />
+          <circle cx="50" cy="0" r="10" fill="#ffbd59" />
+          <circle cx="180" cy="90" r="10" fill="#bf7753" />
+          <circle cx="50" cy="90" r="10" fill="#ffbd59" />
           <text x="50%" y="50%" font-size="8px" text-anchor="middle">
             {{ text }}
           </text>
@@ -33,13 +48,17 @@
 import firebase from "firebase";
 import canvg from "canvg";
 var config = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KRY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  storageBucket: process.env.STRAGE_BUCKET
+  DATABASE_URL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STRAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
+let db;
 if (!firebase.apps.length) {
-  firebase.initializeApp(config);
+  const firebaseApp = firebase.initializeApp(config);
+  db = firebaseApp.firestore();
 }
 
 // svgをjpegに変換
@@ -61,35 +80,30 @@ const svg2imageData = (svgElement, successCallback, errorCallback) => {
     "data:image/svg+xml;charset=utf-8;base64," +
     btoa(unescape(encodeURIComponent(svgData)));
 };
-//終わり
 
 export default {
   data() {
     return {
       text: "クリスマスカード",
-      uuid: "1"
+      description: "大切な人にメッセージカードを贈ろう"
     };
   },
   methods: {
-    // create() {
-    //   var storageRef = firebase.storage().ref();
-    //   var createRef = storageRef.child(`${this.uuid}.jpg`);
-    //   var canvas = document.createElement("canvas");
-    //   var svg = this.$refs.svgArea;
-    //   canvas.width = svg.width.baseVal.value;
-    //   canvas.height = svg.height.baseVal.value;
-    //   const data = new XMLSerializer().serializeToString(this.$refs.svgArea);
-    //   canvg(canvas, data);
-    //   let image = canvas.toDataURL("image/jpeg").split(",")[1];
-    //   createRef.putString(image, "base64").then(snapshot => {
-    //     console.log("Uploaded a blob or file!");
-    //   });
-    // }
-    create() {
-      svg2imageData(this.$refs.svgArea, data => {
+    async create() {
+      svg2imageData(this.$refs.svgArea, async data => {
         const storageRef = firebase.storage().ref();
-        const createRef = storageRef.child("ogp.jpg");
-        createRef.putString(data, "data_url");
+        const createRef = storageRef.child(`${this.text}.jpg`);
+        // Cloud Storageにアップロード
+        await createRef.putString(data, "data_url");
+        const url = await createRef.getDownloadURL();
+        console.log(url);
+
+        // Firestoreに保存しておく
+        const card = db.collection("cards").doc(this.text);
+        await card.set({
+          url,
+          message: this.description
+        });
       });
     }
   }
